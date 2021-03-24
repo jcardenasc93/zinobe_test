0526c471a566e87750cc6d77432043b1a357973d
jcardenasc93 <camilo.cardenas@grability.com>
2021-03-23 20:16:08 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zinobe Challenge
+This project contains the development of the techincal test for Python developer position at Zinobe
---
+++ main.py
@@ -0,0 +1,11 @@
+""" Main project module """
+
+__author__ = "Juan Camilo Cardenas"
+__version__ = "0.1.0"
+
+def main():
+    """ Entry point """
+    print("Python rules")
+
+if __name__ == "__main__":
+    main()
